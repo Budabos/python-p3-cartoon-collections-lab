d3155b4f936438df6821430ea1517e195af14a5f
caroline opiyo <opiyocaroline20@gmail.com>
2023-11-27 22:16:52 +0300

    python-p3-cartoon-collections-lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,19 @@
-def roll_call_dwarves():
-    pass
+# lib/deli_counter.py
 
-def summon_captain_planet():
-    pass
+def roll_call_dwarves(dwarves):
+    for i, dwarf in enumerate(dwarves, start=1):
+        print(f"{i}. {dwarf}")
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return [call.capitalize() + "!" for call in planeteer_calls]
 
-def find_the_cheese():
-    pass
+def long_planeteer_calls(calls):
+    return any(len(call) > 4 for call in calls)
+
+def find_the_cheese(ingredients):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for ingredient in ingredients:
+        if ingredient in cheeses:
+            return ingredient
+    return None
+
